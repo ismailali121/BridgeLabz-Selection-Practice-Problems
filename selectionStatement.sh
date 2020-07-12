1d5b29441e233d09c9bfb2c9441193a48775ea1e
ismailali121 <pathanismailalikhan@gmail.com>
2020-07-13 01:41:45 +0530

    Added program to check if a year is leap year or not

--- selectionStatement.sh
+++ selectionStatement.sh
@@ -1,31 +1,20 @@
-#!/bin/bash
-read -p "enter a date : " date
-echo "enter month number"
-read -p "Example JANUARY=1 | FEBRUARY=2 ..... | DECEMBER=12 : " month
-if [ "$month" -eq 3 ] || [ "$month" -eq 4 ] || [ "$month" -eq 5 ] || [ "$month" -eq 6 ]
-then
-        if [ "$month" -eq 3 ] || [ "$month" -eq 6 ]
-        then
-                if [ "$month" -eq 3 ]
-                then
-                        if [ "$date" -gt "20" ]
-                        then
-                                echo "Given date IS in between March 20 and Jun 20 "
-                        else
-                                echo "Given date IS NOT in between March 20 and Jun 20 "
-                        fi
-                else
-                        if [ "$date" -lt "20" ]
-                        then
-                                echo "Given date IS in between March 20 and Jun 20 "
-                        else
-                                echo "Given date IS NOT in between March 20 and Jun 20 "
-                        fi
-                fi
+#!/bin/bash -x
+echo "Enter the year (YYYY)"
+read year
 
-        else
-                echo "Given date IS in between March 20 and Jun 20 "
-        fi
+if [ $((year % 4)) -eq 0 ]
+then
+  if [ $((year % 100)) -eq 0 ]
+  then
+    if [ $((year % 400)) -eq 0 ]
+    then
+        echo "its a leap year"
+    else
+           echo "its not a leap year"
+    fi
+  else
+  echo "Its a leap year"
+  fi
 else
-        echo "Given date IS NOT in between March 20 and Jun 20 "
+ echo "its not a leap year"
 fi
